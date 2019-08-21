14e5f0ddaaddeb19bc97de45e07f09ead9d35846
Donghyeon Kim <0916dhkim@gmail.com>
2019-08-21 02:34:22 -0400

    Shorter Names for Documentation
    Do not use package/modules names inside documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,9 @@ author = 'Donghyeon Kim'
 # Extensions.
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
+# Autodoc configuration.
+add_module_names = False
+
 # Path configuration.
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
