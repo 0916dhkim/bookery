75ff3367611b7dcbd53aee54eafd2ddfdb531b7d
Donghyeon Kim <0916dhkim@gmail.com>
2019-08-21 02:32:34 -0400

    Use sphinx-rtd-theme for Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,17 @@
+import sphinx_rtd_theme
+
 # Project information.
 project = 'Bookman'
 copyright = '2019, Donghyeon Kim'
 author = 'Donghyeon Kim'
 
 # Extensions.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
 # Path configuration.
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # HTML configuration.
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
