7aa9b5e8ce62988b49bb8ede26cd010cd2bb18dc
Donghyeon Kim <0916dhkim@gmail.com>
2019-09-18 10:47:24 +0900

    Create Custom Signal for Item Double Click Event

--- bookman/widgets/books_page.py
+++ bookman/widgets/books_page.py
@@ -1,14 +1,23 @@
 from PySide2.QtWidgets import QWidget, QVBoxLayout, QLabel, QListView
 from bookman.models import BookModel
+from bookman.persistence import Book
+from PySide2.QtCore import Signal, Slot, QModelIndex
 
 
 class BooksPage(QWidget):
+    book_double_clicked = Signal(Book)
+
     def __init__(self):
         QWidget.__init__(self)
         self._layout = QVBoxLayout(self)
         self._layout.addWidget(QLabel("Books Page"))
         self._view = QListView()
+        self._view.doubleClicked.connect(self.item_double_clicked)
         self._layout.addWidget(self._view)
 
     def set_model(self, model: BookModel):
         self._view.setModel(model)
+
+    @Slot(QModelIndex)
+    def item_double_clicked(self, index: QModelIndex):
+        self.book_double_clicked.emit(index.data())
--- bookman/widgets/members_page.py
+++ bookman/widgets/members_page.py
@@ -1,8 +1,12 @@
 from PySide2.QtWidgets import QWidget, QVBoxLayout, QLabel, QListView
 from bookman.models import MemberModel
+from bookman.persistence import Member
+from PySide2.QtCore import Signal, Slot, QModelIndex
 
 
 class MembersPage(QWidget):
+    member_double_clicked = Signal(Member)
+
     def __init__(self):
         QWidget.__init__(self)
         self._layout = QVBoxLayout(self)
@@ -12,3 +16,7 @@ class MembersPage(QWidget):
 
     def set_model(self, model: MemberModel):
         self._view.setModel(model)
+
+    @Slot(QModelIndex)
+    def item_double_clicked(self, index: QModelIndex):
+        self.member_double_clicked.emit(index.data())
