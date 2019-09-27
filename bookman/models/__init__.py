3f95d01662f3edd669783ea8f91314cac5f8457d
Donghyeon Kim <0916dhkim@gmail.com>
2019-09-27 15:39:17 +0900

    Factor Out Common Parts of Qt Models
    All Qt models for SQLAlchemy mapped classes are now subclass of
    MappedClassModel.

--- bookman/models/__init__.py
+++ bookman/models/__init__.py
@@ -1,5 +1,6 @@
 """Qt models"""
+from .mapped_class_model import MappedClassModel
 from .book_model import BookModel
 from .member_model import MemberModel
 
-__all__ = ["BookModel", "MemberModel"]
+__all__ = ["MappedClassModel", "BookModel", "MemberModel"]
--- bookman/models/book_model.py
+++ bookman/models/book_model.py
@@ -1,115 +1,17 @@
-from PySide2.QtCore import QModelIndex, Qt, QAbstractTableModel
+from bookman.models import MappedClassModel
 from bookman.persistence import Book
-from sqlalchemy.event import listen
-from sqlalchemy import bindparam
-from sqlalchemy.ext import baked
-from sqlalchemy.orm import Session
 from bookman.common import overrides
-from typing import Any, List
-
-bakery = baked.bakery()
 
 
-class BookModel(QAbstractTableModel):
-    """Qt Model for Book Class"""
+class BookModel(MappedClassModel):
+    @overrides(MappedClassModel)
+    def columns(cls):
+        return [Book.title, Book.author, Book.isbn]
 
-    columns = [Book.title, Book.author, Book.isbn]
+    @overrides(MappedClassModel)
+    def id_column(cls):
+        return Book.id
 
-    def __init__(self, session: Session):
-        QAbstractTableModel.__init__(self)
-        self.session = session
-
-        # Cache.
-        self._id_list: List[int] = []
-
-        def query_all_function(session: Session) -> baked.BakedQuery:
-            return session.query(Book)
-
-        self._query_all: baked.BakedQuery = bakery(query_all_function)
-
-        def query_by_id_function(session: Session) -> baked.BakedQuery:
-            return session.query(Book).filter(Book.id == bindparam("id"))
-
-        self._query_by_id: baked.BakedQuery = bakery(query_by_id_function)
-        self._rebuild_cache()
-
-        # Handle SQLAlchemy events.
-        listen(Book, "after_delete", self._sql_after_delete)
-        listen(Book, "after_insert", self._sql_after_insert)
-        listen(Book, "after_update", self._sql_after_update)
-
-    @overrides(QAbstractTableModel)
-    def rowCount(self, parent: QModelIndex = QModelIndex()) -> int:
-        if not parent.isValid():
-            return len(self._id_list)
-        else:
-            return 0
-
-    @overrides(QAbstractTableModel)
-    def columnCount(self, parent: QModelIndex = QModelIndex()) -> int:
-        if not parent.isValid():
-            return len(BookModel.columns)
-        else:
-            return 0
-
-    @overrides(QAbstractTableModel)
-    def data(self, index: QModelIndex, role: int = Qt.DisplayRole) -> Any:
-        if index.isValid() and role in (Qt.DisplayRole, Qt.EditRole):
-            book = (
-                self._query_by_id(self.session)
-                .params(id=self._id_list[index.row()])
-                .one()
-            )
-            assert isinstance(book, Book)
-            return getattr(book, BookModel.columns[index.column()].key)
-        else:
-            return None
-
-    @overrides(QAbstractTableModel)
-    def headerData(
-        self, section: int, orientation: Qt.Orientation, role: int = Qt.DisplayRole
-    ) -> Any:
-        if role == Qt.DisplayRole and orientation == Qt.Horizontal:
-            return BookModel.columns[section].key
-        else:
-            return None
-
-    @overrides(QAbstractTableModel)
-    def setData(self, index: QModelIndex, value: Any, role: int = Qt.EditRole) -> bool:
-        if index.isValid() and role == Qt.EditRole:
-            book = (
-                self._query_by_id(self.session)
-                .params(id=self._id_list[index.row()])
-                .one()
-            )
-            assert isinstance(book, Book)
-            setattr(book, BookModel.columns[index.column()].key, value)
-            return True
-        else:
-            return False
-
-    @overrides(QAbstractTableModel)
-    def flags(self, index: QModelIndex) -> Qt.ItemFlags:
-        if not index.isValid():
-            return Qt.NoItemFlags
-        else:
-            return Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable
-
-    def _rebuild_cache(self):
-        with self.session.no_autoflush:
-            query_result: List[Book] = self._query_all(self.session).all()
-            self._id_list = [x.id for x in query_result]
-
-    def _sql_after_delete(self, mapper, connection, target: Book):
-        index = self._id_list.index(target.id)
-        del self._id_list[index]
-        self.rowsRemoved.emit(QModelIndex(), index, index)
-
-    def _sql_after_insert(self, mapper, connection, target: Book):
-        self._rebuild_cache()
-
-    def _sql_after_update(self, mapper, connection, target: Book):
-        index = self._id_list.index(target.id)
-        self.dataChanged.emit(
-            self.index(index, 0), self.index(index, self.columnCount() - 1)
-        )
+    @overrides(MappedClassModel)
+    def mapped_class(cls):
+        return Book
