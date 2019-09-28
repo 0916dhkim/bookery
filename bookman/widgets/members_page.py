adadf16a64b5d1d70fd43ee08eb00cde519a5621
Donghyeon Kim <0916dhkim@gmail.com>
2019-09-28 20:21:36 +0900

    Add Search Bar for Members

--- bookman/widgets/members_page.py
+++ bookman/widgets/members_page.py
@@ -1,4 +1,11 @@
-from PySide2.QtWidgets import QWidget, QVBoxLayout, QLabel, QHeaderView, QTableView
+from PySide2.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QHeaderView,
+    QTableView,
+    QLineEdit,
+)
 from bookman.models import MemberModel
 from bookman.persistence import Member
 from PySide2.QtCore import Signal, Slot, QModelIndex
@@ -11,6 +18,10 @@ class MembersPage(QWidget):
         QWidget.__init__(self)
         self._layout = QVBoxLayout(self)
         self._layout.addWidget(QLabel("Members Page"))
+
+        self._search_bar = QLineEdit()
+        self._search_bar.textChanged.connect(self.change_search_query)
+        self._layout.addWidget(self._search_bar)
 
         self._view = QTableView()
         self._view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
@@ -26,3 +37,24 @@ class MembersPage(QWidget):
     @Slot(QModelIndex)
     def item_double_clicked(self, index: QModelIndex):
         pass
+
+    @Slot(str)
+    def change_search_query(self, search_query: str):
+        """Update the filter predicate of the model."""
+
+        def predicate(x: Member) -> bool:
+            query_word_set = set(search_query.split())
+            for w in query_word_set:
+                upper_w = w.upper()
+                if all(
+                    [
+                        upper_w not in x.first_name.upper(),
+                        upper_w not in x.last_name.upper(),
+                        upper_w not in (x.last_name + x.first_name).upper(),
+                        upper_w not in x.note.upper(),
+                    ]
+                ):
+                    return False
+            return True
+
+        self._model.predicate = predicate
