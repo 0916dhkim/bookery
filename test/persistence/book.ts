d0655e568f5901306ae0a05e10a49134071f10ce
Donghyeon Kim <0916dhkim@gmail.com>
2019-12-22 16:35:40 -0500

    Add Book Class Test

---
+++ test/persistence/book.ts
@@ -0,0 +1,34 @@
+import { describe, it } from "mocha";
+import * as assert from "assert";
+import { Book, BookSerializer } from "../../src/persistence/book";
+
+function assertBookProperties(
+  book: Book,
+  id: number,
+  title: string,
+  author: string,
+  isbn?: string
+): void {
+  assert.strictEqual(book.id, id);
+  assert.strictEqual(book.title, title);
+  assert.strictEqual(book.author, author);
+  assert.strictEqual(book.isbn, isbn);
+}
+
+describe("Book", function() {
+  describe("BookSerializer", function() {
+    it("Single Book Serialization and Deserialization", function() {
+      const book = new Book(13, "Thirteen", "John Doe", "2382394");
+      const bookSerializer = new BookSerializer();
+      const str = bookSerializer.serialize(book);
+      const deserialized = bookSerializer.deserialize(str);
+      assertBookProperties(
+        deserialized,
+        book.id,
+        book.title,
+        book.author,
+        book.isbn
+      );
+    });
+  });
+});
