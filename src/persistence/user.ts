d19cdb636600628b833899d656391b13fd6e98f9
Donghyeon Kim <0916dhkim@gmail.com>
2019-12-22 20:02:27 -0500

    Replace User Properties With Accessors

--- src/persistence/user.ts
+++ src/persistence/user.ts
@@ -1,10 +1,40 @@
 import { Serializer } from "./serializable";
 
 export class User {
-  public readonly id: number;
-  private lastName: string;
-  private firstName: string;
-  private note: string;
+  private _lastName: string;
+  get lastName(): string {
+    return this._lastName;
+  }
+  set lastName(newLastName: string) {
+    this._lastName = newLastName;
+  }
+
+  private _firstName: string;
+  get firstName(): string {
+    return this._firstName;
+  }
+  set firstName(newFirstName: string) {
+    this._firstName = newFirstName;
+  }
+
+  private _note?: string;
+  get note(): string | undefined {
+    return this._note;
+  }
+  set note(newNote: string | undefined) {
+    this._note = newNote;
+  }
+
+  constructor(
+    readonly id: number,
+    lastName: string,
+    firstName: string,
+    note?: string
+  ) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.note = note;
+  }
 }
 
 export class UserSerializer implements Serializer<User> {
