9b027d7105d38ae5fb429520fb444ae1cff603dc
Donghyeon Kim <0916dhkim@gmail.com>
2019-12-28 11:18:51 -0500

    Add Date Serialization Test

--- test/persistence/view.ts
+++ test/persistence/view.ts
@@ -31,5 +31,28 @@ describe("View", function() {
         view.date
       );
     });
+
+    it("Date Serialization", function() {
+      const view = new View(4, 5, 6, moment.utc(1318781875807));
+      const viewSerializer = new ViewSerializer();
+      const str = viewSerializer.serialize(view);
+      const deserialized = viewSerializer.deserialize(str);
+
+      const year = view.date.utc().year();
+      const month = view.date.utc().month();
+      const date = view.date.utc().date();
+      const hour = view.date.utc().hour();
+      const minute = view.date.utc().minute();
+      const second = view.date.utc().second();
+      const millisecond = view.date.utc().millisecond();
+
+      assert.strictEqual(year, deserialized.date.utc().year());
+      assert.strictEqual(month, deserialized.date.utc().month());
+      assert.strictEqual(date, deserialized.date.utc().date());
+      assert.strictEqual(hour, deserialized.date.utc().hour());
+      assert.strictEqual(minute, deserialized.date.utc().minute());
+      assert.strictEqual(second, deserialized.date.utc().second());
+      assert.strictEqual(millisecond, deserialized.date.utc().millisecond());
+    });
   });
 });
