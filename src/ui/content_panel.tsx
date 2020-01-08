1601ca56cae33e50ff92056743bf513939b79346
Donghyeon Kim <0916dhkim@gmail.com>
2020-01-08 14:00:45 -0500

    Add Document Skeleton

---
+++ src/ui/content_panel.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class ContentPanel extends React.Component<{}, {}> {
+  render(): React.ReactNode {
+    return <div className="js-content-panel">Content Panel</div>;
+  }
+}
