34f7ef1541ef4922566de98161f74b62b82ca934
Donghyeon Kim <0916dhkim@gmail.com>
2020-02-05 07:54:08 -0500

    Export Individual Menu Items

--- src/ui/application_menu.tsx
+++ src/ui/application_menu.tsx
@@ -1,15 +1,21 @@
 import { remote } from "electron";
 
-export const applicationMenu = remote.Menu.buildFromTemplate([
-  {
-    label: "File",
-    submenu: [
-      { label: "Open File..." },
-      { label: "Save" },
-      { label: "Save As..." },
-      { type: "separator" },
-      { role: "quit" }
-    ]
-  },
-  { role: "editMenu" }
-]);
+export const applicationMenu = new remote.Menu();
+
+export const fileMenu = new remote.MenuItem({
+  label: "File",
+  submenu: []
+});
+applicationMenu.append(fileMenu);
+
+export const openFileMenuItem = new remote.MenuItem({ label: "Open File..." });
+fileMenu.submenu.append(openFileMenuItem);
+
+export const saveMenuItem = new remote.MenuItem({ label: "Save" });
+fileMenu.submenu.append(saveMenuItem);
+
+export const saveAsMenuItem = new remote.MenuItem({ label: "Save As..." });
+fileMenu.submenu.append(saveAsMenuItem);
+
+applicationMenu.append(new remote.MenuItem({ role: "editMenu" }));
+applicationMenu.append(new remote.MenuItem({ role: "viewMenu" }));
