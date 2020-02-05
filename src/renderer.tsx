e895799ccfc3cc7daa31c020da3da3acb9a2ea17
Donghyeon Kim <0916dhkim@gmail.com>
2020-02-05 07:53:41 -0500

    Add Main Ref

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -5,8 +5,10 @@ import { remote } from "electron";
 import { Main } from "./ui/main";
 import { applicationMenu } from "./ui/application_menu";
 
+const mainRef = React.createRef<Main>();
+
 $(function() {
-  ReactDOM.render(<Main />, $("#root").get(0));
+  ReactDOM.render(<Main ref={mainRef} />, $("#root").get(0));
 });
 
 remote.Menu.setApplicationMenu(applicationMenu);
