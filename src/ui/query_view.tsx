adbd417f939451b97454cb8d34dd27358800a6e5
Donghyeon Kim <0916dhkim@gmail.com>
2020-02-10 10:04:37 -0500

    Add Basic Query Form

--- src/ui/query_view.tsx
+++ src/ui/query_view.tsx
@@ -3,6 +3,23 @@ import { ContentViewProps } from "./content_view";
 
 export class QueryView extends React.Component<ContentViewProps, {}> {
   render(): React.ReactNode {
-    return <div className="js-query-view"> Query View</div>;
+    return (
+      <div className="js-query-view">
+        <form>
+          <div>
+            <label>
+              Member
+              <input type="text" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Book
+              <input type="text" />
+            </label>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
