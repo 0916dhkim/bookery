ed2d3561b97c38298f3e351a17fec84a6f86d08c
Donghyeon Kim <0916dhkim@gmail.com>
2020-02-14 10:19:19 -0500

    Add Current File Path State

--- src/ui/application_menu.tsx
+++ src/ui/application_menu.tsx
@@ -18,11 +18,13 @@ export function initializeApplicationMenu(main: React.RefObject<Main>): void {
               });
 
               if (result !== undefined && result.length === 1) {
-                const fileContent = fs.readFileSync(result[0], {
+                const filePath = result[0];
+                const fileContent = fs.readFileSync(filePath, {
                   encoding: "utf8"
                 });
                 const appDataSerializer = new AppDataSerializer();
                 main.current.setState({
+                  currentFilePath: filePath,
                   appData: appDataSerializer.deserialize(fileContent)
                 });
               }
