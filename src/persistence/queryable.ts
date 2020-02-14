a97dc77fb7f3dd6ea849fc29612e677ce40dde6f
Donghyeon Kim <0916dhkim@gmail.com>
2020-02-14 06:58:29 -0500

    Remove Queryable Interface

--- src/persistence/queryable.ts
+++
@@ -1,9 +0,0 @@
-export interface Queryable {
-  /**
-   * Tests this instance for fuzzy query string.
-   * Return 0 if the pattern does not match this instance.
-   * Return relevance if the pattern matches.
-   * @param queryString Fuzzy pattern for the query.
-   */
-  query(queryString: string): number;
-}
