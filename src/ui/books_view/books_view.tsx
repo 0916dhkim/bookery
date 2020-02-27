62df1022b2c36a6bdda4b02397b35a447180f434
Donghyeon Kim <0916dhkim@gmail.com>
2020-02-26 19:51:23 -0500

    Add Book Filter

--- src/ui/books_view/books_view.tsx
+++ src/ui/books_view/books_view.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Container, Grid } from "semantic-ui-react";
+import { Container, Grid, Input } from "semantic-ui-react";
 import { BooksList } from "./books_list";
 
 export interface BooksViewProps {
@@ -7,13 +7,23 @@ export interface BooksViewProps {
 }
 
 export function BooksView(): React.ReactElement<BooksViewProps> {
+  const [filterValue, setFilterValue] = React.useState<string>("");
   return (
     <Container fluid>
       Books View
+      <Input
+        type="text"
+        icon="search"
+        value={filterValue}
+        onChange={(event): void => {
+          setFilterValue(event.target.value);
+        }}
+        style={{ flexGrow: 1 }}
+      />
       <Grid divided="vertically">
         <Grid.Column width={8}>
           {/* Books List */}
-          <BooksList />
+          <BooksList filterQuery={filterValue} />
         </Grid.Column>
         <Grid.Column width={8}>{/* Book Edit Form */}</Grid.Column>
       </Grid>
