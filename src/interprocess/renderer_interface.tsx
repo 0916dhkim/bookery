813f1580e62ceed621afdf0ca195a0e301ca62d9
Donghyeon Kim <0916dhkim@gmail.com>
2020-03-03 13:11:20 -0500

    Rename Modules

--- src/interprocess/renderer_interface.tsx
+++
@@ -1,7 +0,0 @@
-import { AppData } from "../persistence/app_data";
-
-export interface RendererInterface {
-  getAppData: () => Promise<AppData>;
-  setAppData: (appData: AppData) => Promise<void>;
-  isModified: () => Promise<boolean>;
-}
