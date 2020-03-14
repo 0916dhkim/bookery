d24a0895e625a3f106352a942a179d11a76996aa
Donghyeon Kim <0916dhkim@gmail.com>
2020-03-14 19:30:50 -0400

    Add Version Number To The Welcome Screen

---
+++ src/renderer/welcome_view.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Request } from "../common/request";
+import { Container } from "semantic-ui-react";
+
+export interface WelcomeViewProps {
+  request: Request;
+}
+
+export function WelcomeView({
+  request
+}: WelcomeViewProps): React.ReactElement<WelcomeViewProps> {
+  const [versionString, setVersionString] = React.useState<string>("");
+  request({ type: "GET-VERSION" }).then(setVersionString);
+  return (
+    <Container fluid>
+      <p>Welcome!</p>
+      <p>Version {versionString}</p>
+    </Container>
+  );
+}
