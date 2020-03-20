de6294c0113083311737e8535ba1d68b365c4522
Donghyeon Kim <0916dhkim@gmail.com>
2020-03-20 19:21:25 -0400

    Change Book And User Filter Implementation

--- src/common/persistence/book_filter.tsx
+++ src/common/persistence/book_filter.tsx
@@ -1,27 +1,18 @@
 import { Filter } from "./filter";
 import { Book } from "./book";
-import * as Fuse from "fuse.js";
-
-const bookFuseOptions: Fuse.FuseOptions<Book> = {
-  shouldSort: true,
-  includeMatches: false,
-  includeScore: false,
-  keys: ["title", "author", "isbn"]
-};
+import { FunctionalIterable } from "./functional_iterable";
+import { matchPattern } from "./match_pattern";
 
 export class BookFilter implements Filter<Book> {
   private books: Array<Book>;
-  private fuse: Fuse<Book, Fuse.FuseOptions<Book>>;
   constructor(data: Iterable<Book>) {
     this.books = Array.from(data);
     this.books.sort((a, b) => b.id - a.id);
-    this.fuse = new Fuse(this.books, bookFuseOptions);
   }
 
   filter(query: string): Iterable<Book> {
-    if (query === "") {
-      return this.books;
-    }
-    return this.fuse.search(query) as Array<Book>;
+    return new FunctionalIterable(this.books).filter(book =>
+      matchPattern(query, ["title", "author", "isbn"], book)
+    );
   }
 }
