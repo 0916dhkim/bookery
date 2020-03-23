cacb0e7d5b18d0163d4339501f806f089d0d9d68
Donghyeon Kim <0916dhkim@gmail.com>
2020-03-23 15:16:42 -0400

    Implement Tagged Query

--- src/common/persistence/filter_book.tsx
+++ src/common/persistence/filter_book.tsx
@@ -2,10 +2,43 @@ import { Book } from "./book";
 import { FunctionalIterable } from "./functional_iterable";
 import { matchPattern } from "./match_pattern";
 import { AppData } from "./app_data";
+import { matchTag } from "./filter_tag";
 
 export function filterBook(appData: AppData, query: string): Iterable<Book> {
-  const filtered = new FunctionalIterable(appData.books.values()).filter(book =>
-    matchPattern(query, ["title", "author", "isbn"], book)
+  // Separate tag query from regular query.
+  const queryWords = query.split(/\s+/);
+  const tagQueries: Array<string> = [];
+  const regularQueries: Array<string> = [];
+  for (const word of queryWords) {
+    if (word.startsWith("#")) {
+      tagQueries.push(word.slice(1));
+    } else {
+      regularQueries.push(word);
+    }
+  }
+  let filtered = new FunctionalIterable(appData.books.values());
+  // Iterate through tag queries.
+  for (const tagQuery of tagQueries) {
+    filtered = filtered.filter(book => {
+      const tagSet = appData.bookTags.get(book.id); // this book's tags.
+      if (!tagSet) {
+        // There is no tag associated with this book.
+        return false;
+      }
+      for (const tagId of tagSet) {
+        const tag = appData.tags.get(tagId);
+        if (tag && matchTag(tag, tagQuery)) {
+          // Found a matching tag.
+          return true;
+        }
+      }
+      // Matching tag not found.
+      return false;
+    });
+  }
+  // Filter by regular query.
+  filtered = filtered.filter(book =>
+    matchPattern(regularQueries.join(""), ["title", "author", "isbn"], book)
   );
   return Array.from(filtered).reverse();
 }
--- src/common/persistence/filter_tag.tsx
+++ src/common/persistence/filter_tag.tsx
@@ -12,15 +12,24 @@ function divideIntoNormalizedSections(s: string): Array<string> {
   return s.toUpperCase().split("/");
 }
 
-export function filterTag(appData: AppData, query: string): Iterable<Tag> {
+/**
+ * @param tag target tag.
+ * @param query query string.
+ * @returns `true` if query matches the given tag. `false` otherwise.
+ */
+export function matchTag(tag: Tag, query: string): boolean {
+  const tagSections = divideIntoNormalizedSections(tag.name);
   const querySections = divideIntoNormalizedSections(query);
-  return new FunctionalIterable(appData.tags.values()).filter(tag => {
-    const tagSections = divideIntoNormalizedSections(tag.name);
-    for (let i = 0; i < querySections.length; i++) {
-      if (i >= tagSections.length || querySections[i] !== tagSections[i]) {
-        return false;
-      }
+  for (let i = 0; i < querySections.length; i++) {
+    if (i >= tagSections.length || querySections[i] !== tagSections[i]) {
+      return false;
     }
-    return true;
-  });
+  }
+  return true;
+}
+
+export function filterTag(appData: AppData, query: string): Iterable<Tag> {
+  return new FunctionalIterable(appData.tags.values()).filter(tag =>
+    matchTag(tag, query)
+  );
 }
