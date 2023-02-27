c47689f78e3e9642228d838aa76c4805b12c1449
Donghyeon Kim <0916dhkim@gmail.com>
2023-02-26 23:19:24 -0500

    Bundle prisma binary

--- app/server/scripts/build.js
+++ app/server/scripts/build.js
@@ -1,8 +1,21 @@
 const ESBuild = require("esbuild");
+const { copy } = require("esbuild-plugin-copy");
 
-ESBuild.buildSync({
+ESBuild.build({
   entryPoints: ["src/index.ts"],
   bundle: true,
   platform: "node",
   outfile: "dist/index.js",
+  sourcemap: true,
+  plugins: [
+    copy({
+      assets: {
+        from: [
+          "node_modules/@bookery/database/src/__generated__/schema.prisma",
+          "node_modules/@bookery/database/src/__generated__/*.node",
+        ],
+        to: ".",
+      },
+    }),
+  ],
 });
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,1 +1,1 @@
-export * from "@prisma/client";
+export * from "./__generated__";
