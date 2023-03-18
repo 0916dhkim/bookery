2a50ceea358c4112111a7bc7afd4a5d4acb3d3f8
Donghyeon Kim <0916dhkim@gmail.com>
2023-03-18 11:10:00 -0400

    Add auth routes

--- app/web/src/main.tsx
+++ app/web/src/main.tsx
@@ -3,11 +3,21 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Root } from "./routes/root";
+import { Signin } from "./routes/signin";
+import { Signup } from "./routes/signup";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+  },
+  {
+    path: "/auth/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/auth/signup",
+    element: <Signup />,
   },
 ]);
 
