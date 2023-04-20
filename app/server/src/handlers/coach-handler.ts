2ac45adc9c913d1c66a87c501f51a8db6d53b4af
Donghyeon Kim <0916dhkim@gmail.com>
2023-04-19 23:47:55 -0400

    Block unauthorized

--- app/server/src/handlers/coach-handler.ts
+++ app/server/src/handlers/coach-handler.ts
@@ -10,6 +10,9 @@ export const getCoachHandler =
       if (id == null) {
         throw new Error("id is missing in URL params.");
       }
+      if (id != req.session.coach?.id) {
+        return res.sendStatus(403);
+      }
       const coach = await coachService.getCoach(id);
       return res.send({ coach });
     } catch (e) {
